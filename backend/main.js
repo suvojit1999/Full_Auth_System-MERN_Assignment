8c6de1c064edf5fa54eddfe2c2cb38fb0ea6ec66
suvojit1999 <164034159+suvojit1999@users.noreply.github.com>
2024-09-29 19:24:54 +0530

    test 2

--- backend/main.js
+++ backend/main.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import auth from './apis/auth.js'
 import 'dotenv/config'
+import axios from 'axios'
 
 const app = express()
 const port = process.env.PORT || 3000
